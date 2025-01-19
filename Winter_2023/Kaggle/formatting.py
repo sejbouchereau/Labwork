a0854082dbdb1b1e2f6f0bfb5857c6466e759cfa
Samuel <sejbouchereau@gmail.com>
2025-01-19 12:31:26 -0500

    French to English translation

--- Winter_2023/Kaggle/formatting.py
+++ Winter_2023/Kaggle/formatting.py
@@ -1,33 +1,33 @@
 import pandas as pd
 import os
 
-# Chemin vers le dossier contenant le Dataset (à changer selon l'emplacement de vos fichiers locaux)
+# Path to the folder containing the Dataset (change it according to the location of your local files)
 path = r"C:\Users\sejbo\.kaggle\420-318-ah-a24\Bitbrains-fastStorage\2013-8"
 output_data_file = r"C:\Users\sejbo\.kaggle\420-318-ah-a24\dataset.csv"
 output_sample_file = r"C:\Users\sejbo\.kaggle\420-318-ah-a24\sample.csv"
 
-# Lister tous les fichiers .csv
+# List all .csv files
 csv_files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.csv')]
 
-# Charger les fichiers .csv
+# Load the .csv files
 dfs = []
 for file in csv_files:
     df = pd.read_csv(file, sep=';')
     dfs.append(df)
 
-# Combiner tous les DataFrames
+# Combine all DataFrames
 combined_data = pd.concat(dfs, ignore_index=True)
 
-# Formater les colonnes
+# Format the columns
 combined_data.columns = [col.strip().replace('\t', '') for col in combined_data.columns]
 
-# Sauvegarder localement l'intégralité des données
+# Save all data locally
 combined_data.to_csv(output_data_file, index=False)
 
-# Utiliser uniquement les 200k premières lignes du Dataset
+# Use only the first 200,000 rows of the Dataset
 data_sample = combined_data.head(200000)
 
-# Sauvegarder localement le sample
+# Save the sample locally
 data_sample.to_csv(output_sample_file, index=False)
 
-print(f"Fichiers sauvagardés à l'emplacement : {output_data_file, output_sample_file}")
+print(f"Files saved at: {output_data_file, output_sample_file}")

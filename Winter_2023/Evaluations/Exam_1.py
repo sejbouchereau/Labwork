2b37ff84c5f29bbc7c17ddf812444a0247e91d5d
sejbouchereau <sejbouchereau@gmail.com>
2025-02-27 18:38:42 -0500

    French to English translation

--- Winter_2023/Evaluations/Exam_1.py
+++ Winter_2023/Evaluations/Exam_1.py
@@ -3,69 +3,69 @@ Question 1.
 """
 from math import *
 
-# TODO Répondez à la question 1 ci-dessous
+# TODO Answer question 1 below
 
-# a) Faux
-# b) Vrai
-# c) Faux
-# d) Vrai
-# e) Vrai
-# f) Vrai
-# g) Vrai
-# h) Faux
+# a) False
+# b) True
+# c) False
+# d) True
+# e) True
+# f) True
+# g) True
+# h) False
 
 """
 Question 2:
 """
 
-# TODO Répondez à la question 2 ci-dessous
+# TODO Answer question 2 below
 
 
-def est_triangle(a, b, c):
+def is_triangle(a, b, c):
     if min(a, b, c) <= 0:
-        raise ValueError("Les valeurs doivent être des entiers positifs.")
+        raise ValueError("Values must be positive integers.")
     return a + b > c
 
 
-print(est_triangle(1, 2, 3))
+print(is_triangle(1, 2, 3))
 
 """
 Question 3:
 """
 
-# TODO Répondez à la question 3 ci-dessous
+# TODO Answer question 3 below
 
 
 class Rectangle:
-    def __init__(self, longueur: int, largeur: int):
-        self.longueur = int(longueur)
-        self.largeur = int(largeur)
+    def __init__(self, length: int, width: int):
+        self.length = int(length)
+        self.width = int(width)
 
-    def aire(self):
-        aire = (self.longueur * self.largeur)
-        return aire
+    def area(self):
+        area = (self.length * self.width)
+        return area
 
-    def perimetre(self):
-        perimetre = (self.longueur * 2) + (self.largeur * 2)
-        return perimetre
+    def perimeter(self):
+        perimeter = (self.length * 2) + (self.width * 2)
+        return perimeter
 
-    def est_carre(self):
-        if self.longueur == self.largeur:
+    def is_square(self):
+        if self.length == self.width:
             return True
         else:
             return False
 
 
 rec = Rectangle(2, 4)
-print(rec.aire())
-print(rec.perimetre())
-print(rec.est_carre())
+print(rec.area())
+print(rec.perimeter())
+print(rec.is_square())
 
 """
 Question 4
 """
 
-# TODO Répondez à la question 4 ci-dessous
+# TODO Answer question 4 below
 
 
 class Point:
@@ -73,25 +73,25 @@ class Point:
         self.x = x
         self.y = y
 
-    def distance(self, autre_point):
-        return sqrt((self.x - autre_point.x) ** 2 + (self.y - autre_point.y) ** 2)
+    def distance(self, other_point):
+        return sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
 
-    def deplacer(self, deplacement_x, deplacement_y):
-        self.x += deplacement_x
-        self.y += deplacement_y
+    def move(self, move_x, move_y):
+        self.x += move_x
+        self.y += move_y
 
-    def afficher_position(self):
+    def display_position(self):
         return f"({self.x},{self.y})"
 
 
 p1 = Point(1, 4)
 p2 = Point(1, 5)
 
-print(f"Distance p1-p2 est de: {p1.distance(p2)}")
+print(f"Distance p1-p2 is: {p1.distance(p2)}")
 
-p1.deplacer(0, 1)
+p1.move(0, 1)
 
-print(f"Après le déplacement, la distance p1-p2 est de: {p1.distance(p2)}")
+print(f"After moving, the distance p1-p2 is: {p1.distance(p2)}")
 
-print(f"La position de p1: {p1.afficher_position()}")
-print(f"La position de p2: {p2.afficher_position()}")
+print(f"Position of p1: {p1.display_position()}")
+print(f"Position of p2: {p2.display_position()}")

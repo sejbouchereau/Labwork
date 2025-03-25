5a6629e7bd4d4bb6dbc872f13fa3fa7e66a910f3
sejbouchereau <sejbouchereau@gmail.com>
2025-03-25 19:57:50 -0400

    French to English translation

--- Winter_2023/Projects/Bank/bank_account.py
+++ Winter_2023/Projects/Bank/bank_account.py
@@ -1,16 +1,16 @@
-class CompteBancaire:
-    def __init__(self, nom, solde, numero_de_compte):
-        self.nom = str(nom)
-        self.solde = int(solde)
-        self.numero_de_compte = int(numero_de_compte)
-    
-    def deposer(self, montant):
-        self.solde += montant
+class BankAccount:
+    def __init__(self, name, balance, account_number):
+        self.name = str(name)
+        self.balance = int(balance)
+        self.account_number = int(account_number)
 
-    def retirer(self, montant):
-        if montant > self.solde:
-            raise ValueError("Montant supérieur au solde du compte.")
-        self.solde -= montant
+    def deposit(self, amount):
+        self.balance += amount
 
-    def afficher_solde(self):
-        print(f"Solde au compte : {self.solde}$")
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise ValueError("Amount exceeds account balance.")
+        self.balance -= amount
+
+    def display_balance(self):
+        print(f"Account balance: {self.balance}$")
